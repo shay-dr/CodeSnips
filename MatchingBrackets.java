7b6933e538ec8c0a3e3e360b273029a013375539
David Shay <shay.davidr@gmail.com>
2021-09-24 16:54:48 -0600

    Added for loops, pausing work.

--- MatchingBrackets.java
+++ MatchingBrackets.java
@@ -1,6 +1,5 @@
 import org.jetbrains.annotations.NotNull;
 
-import java.util.regex.Pattern;
 
 public class MatchingBrackets {
 
@@ -8,20 +7,46 @@ public class MatchingBrackets {
 
         int countLeft = 0;
         int countRight = 0;
+        char leftBracket = '{';
+        char rightBracket = '}';
+        char response = 'f';
 
-        //Obtain string and scan / count bracket {,}.
+
+        //Obtain string, scan for, and count brackets {,}.
         char[] letterPlace = input.toCharArray();
 
         for (char slotValue : letterPlace) {
             System.out.println(slotValue);
+            if (slotValue == leftBracket)
+            {
+                countLeft ++;
+            }
+            if (slotValue == rightBracket)
+            {
+                countRight ++;
+            }
+
         }
 
-        //If all brackets have a matching pair, return true.  Else, return false.
+        //If all brackets have a matching pair, response equals true.
+        if (countLeft == countRight) {
+            response = 't';
+        }
 
+        //Verify that left bracket comes before right bracket.  --Needs work!
+
+        for (char slotValue : letterPlace) {
+            System.out.println(slotValue);
+            if (slotValue == rightBracket  && slotValue < the slot value for leftBracket)
+            {
+                response ='f';
+            }
+        }
 
         //Response - update this to whatever the logic provides (not hard coded true).  Need to have order and matching as well.
-        return true;
-
-
+        if (response == 't')
+            return true;
+        if (response == 'f')
+            return false;
     }
 }
