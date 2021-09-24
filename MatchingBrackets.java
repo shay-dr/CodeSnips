a6b375d5fb5d6c042108af09f7bfd08f721fd467
David Shay <shay.davidr@gmail.com>
2021-09-23 22:08:39 -0600

    switched from regex to toCharArray

--- MatchingBrackets.java
+++ MatchingBrackets.java
@@ -1,5 +1,3 @@
-package codeTests;
-
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -9,36 +7,26 @@ public class MatchingBrackets {
 
         int countLeft = 0;
         int countRight = 0;
-        string response = null;
 
 
         //Init post
-        System.out.println("Thank you for using MatchingBrackets /n" + "Please provide a string to be tested and press the 'Enter' key:");
+        System.out.println("Thank you for using MatchingBrackets \n" + "Please provide a string to be tested and press the 'Enter' key:");
         Scanner input = new Scanner(System.in);
 
-        //Obtain string and scan / count bracket {,}.  Regex: /\{|\}/g
+        //Obtain string and scan / count bracket {,}.
         String userInput = input.next();
+        char[] letterPlace = userInput.toCharArray();
 
-        Pattern regex = Pattern.compile("{}");
-
-        if (regex.matcher(userInput).find({)) {
-            countLeft ++;
-
+        for (char slotValue : letterPlace) {
+            System.out.println(slotValue);
         }
-        if (regex.matcher(userInput).find(})) {
-            countRight ++;
 
-        }
 
         //If all brackets have a matching pair, return true.  Else, return false.
 
-        if countLeft = countRight(
-                response  = True
-        )
 
         //Response
 
-        System.out.println(response);
 
     }
 }
