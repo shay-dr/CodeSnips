0979ef20a67cb3928fc398d482e5dc47f29f8eba
David Shay <shay.davidr@gmail.com>
2021-09-24 20:11:10 -0600

    Removed old notes and unused variables.

--- MatchingBrackets.java
+++ MatchingBrackets.java
@@ -5,12 +5,9 @@ public class MatchingBrackets {
 
     public boolean isMatch(@NotNull String input) {
 
-        int countLeft = 0;
-        int countRight = 0;
         int bracketCounter = 0;
         char leftBracket = '{';
         char rightBracket = '}';
-        char response = 'f';
         Boolean result = null;
 
 
@@ -36,42 +33,13 @@ public class MatchingBrackets {
             if (slotValue == leftBracket) {
                 bracketCounter++;
             }
-
         }
 
-
-/*            if (slotValue == leftBracket)
-            {
-                countLeft ++;
-            }
-            if (slotValue == rightBracket)
-            {
-                countRight ++;
-            }
-
-        }
-*/
         //If all brackets have a matching pair, response equals true.
         if (bracketCounter == 0) {
-//          response = 't';
             result = true;
         }
 
-        //Verify that left bracket comes before right bracket.  --Needs work!
-/*
-        for (char slotValue : letterPlace) {
-            System.out.println(slotValue);
-            if (slotValue == rightBracket  && slotValue < the slot value for leftBracket)
-            {
-                response ='f';
-            }
-        }
-*/
-        //Response - update this to whatever the logic provides (not hard coded true).  Need to have order and matching as well.
-//        if (response == 't')
-//            return true;
-//        if (response == 'f')
-//            return false;
         return result;
     }
 }
